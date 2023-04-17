65dfb9c64bde8e771e0b0346e313254c7a68f9f9
Hironori Kuwahara <kuwanori4778@gmail.com>
2023-04-17 23:12:18 +0900

    Initial commit for traframe.

---
+++ src/main/java/com/traframe/TraframeApplication.java
@@ -0,0 +1,13 @@
+package com.traframe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraframeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraframeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/traframe/TraframeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.traframe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraframeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
